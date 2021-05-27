11f1b5b3c74b43e5b3eb6f9796cb0d5df6e666a5
Tash Ramsden <natasha.ramsden18@gmail.com>
2021-05-27 12:43:17 +0100

    Change to os.getenv()

--- main.py
+++ main.py
@@ -12,7 +12,8 @@ from functools import wraps
 import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.environ["SECRET_KEY"]
+# app.config['SECRET_KEY'] = os.environ["SECRET_KEY"]
+app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
